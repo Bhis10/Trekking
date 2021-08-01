774c32485ac389b99390886b55ecefc7f9df3e8d
Bhis10 <bhisma.mgr@gmail.com>
2021-08-01 16:28:27 +0100

    remove

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
 
 
- const form = document.getElementById('form');
- const username = document.getElementById('username');
- const fullname = document.getElementById('fullname');
- const email = document.getElementById('email');
- const password = document.getElementById('password');
-
- function on() {
-  document.getElementById("banner-img").style.display = "block";
-}
-
- form.addEventListener('submit', e => {
- 	e.preventDefault();
-
- 	checkInputs();
- })
- function checkInputs() {
-
-
- 	if(usernameValue === '') {
- 		setErrorFor(username, 'Username cannot be blank');
- 	} else {
- 		setSuccessFor(username);
- 	}
- 	if(passwordValue === '') {
- 		setErrorFor(password, 'Password cannot be blank');
- 	} else {
- 		setSuccessFor(password);
-
- 	}
+ 
+	 
